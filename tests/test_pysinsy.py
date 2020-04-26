ee9cf5836d3d8032da8575362bbb57a4fddd1524
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-04-26 21:22:11 +0900

    Add test

---
+++ tests/test_pysinsy.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from os.path import dirname, join
+import pysinsy
+
+
+DATA_DIR = dirname(__file__)
+
+def test_pysinsy():
+    # http://sinsy.sp.nitech.ac.jp/sample/song070_f00001_063.xml
+
+    xml_path = join(DATA_DIR, "song070_f00001_063.xml")
+
+    sinsy = pysinsy.sinsy.Sinsy()
+    assert sinsy.setLanguages("j", "/usr/local/lib/sinsy/dic")
+    assert sinsy.loadScoreFromMusicXML(xml_path)
+
+    is_mono = True
+    for is_mono in [True, False]:
+        label = sinsy.createLabelData(is_mono, 1, 1)
+        for l in label.getData():
+            print(l)
+
+    sinsy.clearScore()
+
